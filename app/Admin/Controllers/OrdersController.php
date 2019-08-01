18afb3f90b30d2280b1733208313420496809972
ffsong <cq_tans@163.com>
2019-08-01 16:02:42 +0800

    管理后台 - 订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -2,12 +2,15 @@
 
 namespace App\Admin\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Order;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
+use Illuminate\Http\Request;
+use Validator;
 
 class OrdersController extends AdminController
 {
@@ -71,63 +74,36 @@ class OrdersController extends AdminController
             ->body(view('admin.orders.show', ['order' => $order]));
     }
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
+    // 发货
+    public function ship(Order $order, Request $request)
     {
-        $show = new Show(Order::findOrFail($id));
+        // 判断当前订单是否已支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未付款');
+        }
+        // 判断当前订单发货状态是否为未发货
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已发货');
+        }
 
-        $show->field('id', __('Id'));
-        $show->field('no', __('No'));
-        $show->field('user_id', __('User id'));
-        $show->field('address', __('Address'));
-        $show->field('total_amount', __('Total amount'));
-        $show->field('remark', __('Remark'));
-        $show->field('paid_at', __('Paid at'));
-        $show->field('payment_method', __('Payment method'));
-        $show->field('payment_no', __('Payment no'));
-        $show->field('refund_status', __('Refund status'));
-        $show->field('refund_no', __('Refund no'));
-        $show->field('closed', __('Closed'));
-        $show->field('reviewed', __('Reviewed'));
-        $show->field('ship_status', __('Ship status'));
-        $show->field('ship_data', __('Ship data'));
-        $show->field('extra', __('Extra'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
+        $data = Validator::validate($request->all(),[
+            'express_company' => ['required'],
+            'express_no'      => ['required'],
+        ],[],[
+            'express_company' => '物流公司',
+            'express_no'      => '物流单号',
+        ]);
 
-        return $show;
+        // 将订单发货状态改为已发货，并存入物流信息
+        $order->update([
+            'ship_status' => Order::SHIP_STATUS_DELIVERED,
+            // 我们在 Order 模型的 $casts 属性里指明了 ship_data 是一个数组
+            // 因此这里可以直接把数组传过去
+            'ship_data'   => $data,
+        ]);
+
+        // 返回上一页
+        return redirect()->back();
     }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new Order);
-
-        $form->text('no', __('No'));
-        $form->number('user_id', __('User id'));
-        $form->text('address', __('Address'));
-        $form->decimal('total_amount', __('Total amount'));
-        $form->textarea('remark', __('Remark'));
-        $form->datetime('paid_at', __('Paid at'))->default(date('Y-m-d H:i:s'));
-        $form->text('payment_method', __('Payment method'));
-        $form->text('payment_no', __('Payment no'));
-        $form->text('refund_status', __('Refund status'))->default('pending');
-        $form->text('refund_no', __('Refund no'));
-        $form->switch('closed', __('Closed'));
-        $form->switch('reviewed', __('Reviewed'));
-        $form->text('ship_status', __('Ship status'))->default('pending');
-        $form->textarea('ship_data', __('Ship data'));
-        $form->textarea('extra', __('Extra'));
-
-        return $form;
-    }
 }
