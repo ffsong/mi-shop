1de50815bce231f947ca66a2ed0daf0d7b19ad1d
ffsong <cq_tans@163.com>
2019-08-05 11:31:57 +0800

    管理后台 - 拒绝退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -28,5 +28,8 @@ Route::group([
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 
+    // 拒绝退款
+    $router->post('orders/{order}/refund','OrdersController@handleRefund')->name('admin.orders.handle_refund');
+
 
 });
